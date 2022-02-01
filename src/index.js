7740b593a6c6d95b4c845066eab6742fa90c3a46
Ash Anand <cppanand@gmail.com>
2022-02-01 15:30:07 -0500

    Add Sentry SDK

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import '@babel/polyfill';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
+
 import server from './server';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+});
 
 server.start({ port: process.env.PORT || 4000 }, () => {
   console.log('The server is up!');
 });
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
