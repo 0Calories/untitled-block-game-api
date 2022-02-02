896089dd84870ee05f58cc008df1a4f7fc1f18c6
Ash Anand <cppanand@gmail.com>
2022-02-02 10:24:34 -0500

    Add HTTP tracing

--- src/index.js
+++ src/index.js
@@ -7,23 +7,11 @@ import server from './server';
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   tracesSampleRate: 1.0,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+  ],
 });
 
 server.start({ port: process.env.PORT || 4000 }, () => {
   console.log('The server is up!');
 });
-
-const transaction = Sentry.startTransaction({
-  op: "test",
-  name: "My First Test Transaction",
-});
-
-setTimeout(() => {
-  try {
-    foo();
-  } catch (e) {
-    Sentry.captureException(e);
-  } finally {
-    transaction.finish();
-  }
-}, 99);
